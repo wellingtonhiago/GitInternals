4df7a2041dbe8d465dd0d25861b2f574463590b6
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-09-21 18:57:01 -0300

    Stage 2/7 - Tipos de objetos Git (Refatorado)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,19 @@
+import java.io.File
 import java.io.FileInputStream
 import java.util.zip.InflaterInputStream
 
 fun main() {
-    println("Enter .git directory location:")
-    val directory = readln()
-    println("Enter git object hash:")
-    val hash = readln()
+    val dir = println("Enter .git directory location:").run { readln() }
+    val hash = println("Enter git object hash:").run { readln() }
 
-    val path = "$directory/objects/${hash.substring(0, 2)}/${hash.substring(2)}"
-    val byteArray = InflaterInputStream(FileInputStream(path)).readAllBytes()
+    val fileDir = dir.plus("/objects/${hash.take(2)}/${hash.drop(2)}")
 
-    val headerEnd = byteArray.indexOfFirst { it.toInt() == 0 }
-    val fullHeader = byteArray
-        .filterIndexed { index, _ -> index in 0 until headerEnd }
-        .map { it.toInt().toChar() }
-        .joinToString("")
-
-    val (type, length) = fullHeader.split(" ")
-    println("type:$type length:$length")
+    InflaterInputStream(FileInputStream(File(fileDir)))
+        .readBytes()
+        .decodeToString()
+        .split('\u0000')[0]
+        .split(' ')
+        .let {
+            print("type:${it[0]} length:${it[1]}")
+        }
 }
